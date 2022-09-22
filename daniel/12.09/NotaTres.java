becc0fc891a9b9c45ced461c0d2cd3d7b7578c38
Marcus Del Pino <marcusdelpino14@gmail.com>
2022-09-21 22:21:59 -0300

    Aula sobre array

--- daniel/12.09/NotaTres.java
+++ daniel/12.09/NotaTres.java
@@ -6,11 +6,13 @@ public class NotaTres {
 
         Scanner leitor = new Scanner(System.in);
         double n1, n2, n3;
+
+
 /* 
 n1 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota laboratorial?"));
 n2 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota semestral?"));
 n3 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota da avaliacao final?"));
-*/  
+*/
         System.out.println("Qual a nota laboratorial?");
         n1 = leitor.nextFloat();
         System.out.println("Qual a nota semestral?");
@@ -19,17 +21,19 @@ n3 = Double.parseDouble(JOptionPane.showInputDialog("Qual a nota da avaliacao fi
         n3 = leitor.nextFloat();
 
         if (n1 > 2) {
+            boolean parar = false;
+
+            if (parar == false) {
+                System.out.println("O valor apresentado esta incorreto, reveja as notas e as insira novamente!");
+            }
+            //break;
+        } else if (n2 > 3) {
             System.out.println("O valor apresentado esta incorreto, reveja as notas e as insira novamente!");
-            
-        }
-        else if (n2 > 3){
+        } else if (n3 > 5) {
             System.out.println("O valor apresentado esta incorreto, reveja as notas e as insira novamente!");
-        }
-        else if (n3 > 5) {
-            System.out.println("O valor apresentado esta incorreto, reveja as notas e as insira novamente!");
-            
-        }else {
-            System.out.println("A nota final e: " + (n1+n2+n3));
+
+        } else {
+            System.out.println("A nota final e: " + (n1 + n2 + n3));
         }
 
         leitor.close();
