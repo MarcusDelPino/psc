8c5e5e0e022ade5c6d151a0003965d8bead6bc98
Marcus Del Pino <marcusdelpino14@gmail.com>
2022-09-24 10:50:06 -0300

    ArrayList

---
+++ loiane/src/ArrayListClasses.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ArrayListClasses {
+    public static void main(String[] args) {
+
+       // Pessoas pessoas = new Pessoas("Marcus", 30);
+
+        Collections.sort();
+        Scanner ler = new Scanner(System.in);
+        ArrayList<Pessoas> pessoa = new ArrayList<Pessoas>();
+
+        pessoa.add(new Pessoas(ler.next(), ler.nextInt()));
+    }
+}
---
+++ loiane/src/Pessoas.java
@@ -0,0 +1,27 @@
+public class Pessoas {
+    private String nomes;
+    private int idades;
+
+    public Pessoas(String nomes, int idades) {
+        this.nomes = nomes;
+        this.idades = idades;
+    }
+
+    public String getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String nomes) {
+        this.nomes = nomes;
+    }
+
+    public int getIdades() {
+        return idades;
+    }
+
+    public void setIdades(int idades) {
+        this.idades = idades;
+    }
+}
+
+
